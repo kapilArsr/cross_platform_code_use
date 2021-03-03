693f1d25e9c7669c72b0afc70fb513ae639a8927
kapilbansal <kapil.bansal@arsrgroup.com>
2021-03-03 11:59:07 +0530

    Initial commit

---
+++ app/src/main/kotlin/com/health/cross_platform_code_use/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.health.cross_platform_code_use
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+
+
+class MainActivity: FlutterActivity() {
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "battery").setMethodCallHandler { call, result ->
+
+            if(call.method == "getBatteryLevel"){
+                val batterLevel = getBatteryLevel()
+
+                if(batterLevel != -1){
+                    result.success(batterLevel)
+                }else{
+                    result.error("Unavailable", "BatteryLevel is not available",null)
+                }
+            }
+        }
+
+    }
+
+
+    private fun getBatteryLevel() : Int {
+        val batteryLevel: Int
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
+    }
+
+
+}
